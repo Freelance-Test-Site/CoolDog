874fa09f32128e8b7c0c9a28f636e8964a9cdfdd
VadimVorkuta <126154146+VadimVorkuta@users.noreply.github.com>
2023-11-12 10:34:50 +0300

    Add files via upload

---
+++ js/scriptsliderconfig.js
@@ -0,0 +1,60 @@
+"use strict"
+/************************************************************************Slider-Swiper**************************************************************************************/
+new Swiper('.review-slider', {
+
+
+	// navigation: {
+	// 	nextEl: '.swiper-button-next',
+	// 	prevEl: '.swiper-button-prev'
+	// },
+	// pagination: {
+	// 	el: '.swiper-pagination',
+	// 	clickable: true,
+	// },
+
+	simulateTouch: true,
+	touchRatio: 1,
+	touchAngle: 90,
+	grabCursor: true,
+
+	slideToClickedSlide: true,
+
+	hashNavigation: {
+		watchState: true,
+	},
+
+	keyboard: {
+		enable: true,
+		onlyInViewport: true,
+		pageUpDown: true,
+	},
+	autoHeight: true,
+
+	watchOverflow: true,
+
+	spaceBetween: 20,
+
+	// slidesPrGroup: 3,
+
+	centeredSlides: true,
+
+	initialSlide: 1,
+
+	// loop: true,
+	// loopedSlides: 2,
+
+	freeMode: true,
+
+	breakpoints:{
+		1366:{
+			slidesPerView: 3.2,
+		},
+		1024:{
+			slidesPerView: 2.2,
+		},
+		768:{
+			slidesPerView: 1.2,
+		},
+	}
+});
+/*******************************************************************************************/
